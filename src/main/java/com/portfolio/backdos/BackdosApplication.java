87a6abddebfac304386577f00e1bbf84130e6ce6
richardfffff <richardmace6@gmail.com>
2023-02-15 13:46:42 -0300

    generacion del back del portfolio

---
+++ src/main/java/com/portfolio/backdos/BackdosApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.backdos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackdosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackdosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/backdos/BackdosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.backdos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackdosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
